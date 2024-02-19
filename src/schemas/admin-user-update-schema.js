75fe48860de02059dad621552dba7a90b5855722
ashibtechversant <ashibrehmanb@techversantinfotech.com>
2024-02-19 09:59:17 +0530

    Schema changes

--- src/schemas/admin-user-update-schema.js
+++ src/schemas/admin-user-update-schema.js
@@ -4,7 +4,6 @@ module.exports = Joi.object({
   fullName: Joi.string().min(3),
   email: Joi.string().email().lowercase(),
   role: Joi.string().valid('admin', 'agent', 'supervisor', 'qc', 'qa'),
-  profilePictureUrl: Joi.string(),
   password: Joi.string()
     .min(8)
     .pattern(/^[a-zA-Z0-9]{3,30}$/),
