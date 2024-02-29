379da52d5efb53dfd40e242d051cdf453366b4a2
ashibtechversant <ashibrehmanb@techversantinfotech.com>
2024-02-29 11:06:23 +0530

    Update: Error for Multiple image upload
    - Update: Documentation for profile-picture upload
    - Update: Multer-error-middleware show updated message during ExpectationFailed error

--- src/middleware/multer-error-middleware.js
+++ src/middleware/multer-error-middleware.js
@@ -4,9 +4,11 @@ const multer = require('multer');
 module.exports = async (err, _, __, next) => {
   try {
     if (err instanceof multer.MulterError)
-      throw createHttpError.ExpectationFailed('expected field not found');
+      throw createHttpError.ExpectationFailed(
+        'expected field not found or multiple files uploaded'
+      );
     if (err.message === 'Unexpected end of form')
-      throw createHttpError.BadRequest(err.message);
+      throw createHttpError.BadRequest(err.message + err.code);
     if (err.message === 'Input buffer contains unsupported image format')
       throw createHttpError.UnsupportedMediaType(err.message);
     throw err;
